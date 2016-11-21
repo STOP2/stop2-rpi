7c43fb52026bd934d42c86c1336760416ceee017
Ville Talonpoika <talonvi@gmail.com>
2016-11-21 11:28:21 +0200

    UTF-8

--- term/Main.py
+++ term/Main.py
@@ -1,8 +1,10 @@
+# -*- coding: utf-8 -*-
+
 from term.rpi_controller import RPIController
 
 # Initialization
 
-s = input('Laitetaanko valo päälle? ')
+s = input('Laitetaanko valo paalle? ')
 if s:
     RPIController.turnLightOn()
 else:
