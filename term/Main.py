9699c8473662de308d759b28b09dcd74a3c71dc5
Ville Talonpoika <talonvi@gmail.com>
2016-11-26 11:33:40 +0200

    MockRPIController started

--- term/Main.py
+++ term/Main.py
@@ -1,22 +1,29 @@
 # -*- coding: utf-8 -*-
 
+RPI_MODE = False
+
 import sys
 import configparser
-from rpi_controller import RPIController
-from mqtt_controller import MQTTManager
+if (RPI_MODE):
+    from rpi_controller import RPIController
+from mqtt_controller import MQTTController
 
 # Initialization
-
-rpi = RPIController()
-mqtt = MQTTManager()
-
 config = configparser.ConfigParser()
 config.read('../config.ini')
 hslApi = config.get('API', 'HSL_API')
 print(hslApi)
 
+if (RPI_MODE):
+    rpi = RPIController()
+else:
+    rpi = MockRPIController()
+
+
+mqtt = MQTTController(sys.argv[0]) # Vehicle ID
+
 # Connect to MQTT by bus number
-mqtt.connect(sys.argv[0])
+mqtt.connect(config.get('API', 'MQTT_ADDRESS'), config.get('API', 'MQTT_PORT'))
 
 # noinspection PyBroadException
 try:
--- term/mqtt_controller.py
+++ term/mqtt_controller.py
@@ -3,21 +3,21 @@
 import paho.mqtt.client as mqtt
 
 
-class MQTTManager:
+class MQTTController:
 
-    def connect(self):
+    def __init__(self, veh_id):
+        self.veh_id = veh_id
+
+    def connect(self, broker_url, broker_port):
         print("Connecing to MQTT broker")
         client = mqtt.Client()
         client.on_connect = self.on_connect
         client.on_message = self.on_message
-        client.connect("ws://epsilon.fixme.fi", 9001, 60)
+        client.subscribe("stoprequests2/" + self.veh_id)
+        client.connect(broker_url, broker_port, 60)
 
     def on_connect(client, userdata, flags, rc):
         print("Connected with result code " + str(rc))
-
-        # Subscribing in on_connect() means that if we lose the connection and
-        # reconnect then subscriptions will be renewed.
-        client.subscribe("$SYS/#")
 
     # The callback for when a PUBLISH message is received from the server.
     def on_message(client, userdata, msg):
